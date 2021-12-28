bdb593ebf3773ebc896837062930ffde3f26e37a
Arif Hidayat <moharifhidayat7@outlook.com>
2021-12-28 11:45:42 +0700

    asa

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://api.rumahdigitalgombengsari.com',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '37d7fbf4a2b1af6aaaad02ee71111f2a'),
@@ -9,3 +8,4 @@ module.exports = ({ env }) => ({
   },
 });
 
+  // url: 'https://api.rumahdigitalgombengsari.com',
