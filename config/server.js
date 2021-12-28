7cb457ce5ab7355dd6fdfeb11886288e49101c26
Arif Hidayat <moharifhidayat7@outlook.com>
2021-12-28 13:13:34 +0700

    asd

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: '/api',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '37d7fbf4a2b1af6aaaad02ee71111f2a'),
