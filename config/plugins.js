0f8f07ed9b31baa36beb314df82bbfa4748e896b
root <root@rumahdigitalgombengsari.com>
2022-02-07 05:31:19 +0100

    tes

--- config/plugins.js
+++ config/plugins.js
@@ -2,16 +2,16 @@ module.exports = ({ env }) => ({
     // ...
     email: {
       provider: env('EMAIL_PROVIDER'),
-      providerOptions: {
-        host: env('EMAIL_SMTP_HOST', 'smtp.example.com'),
-        port: env('EMAIL_SMTP_PORT', 25),
-        secure: env('EMAIL_SMTP_SECURE', false),
-        auth: {
-          user: env('EMAIL_SMTP_USER'),
-          pass: env('EMAIL_SMTP_PASS'),
-        },
-        ignoreTLS: true,
-      },
+    //   providerOptions: {
+    //     host: env('EMAIL_SMTP_HOST', 'smtp.example.com'),
+    //     port: env('EMAIL_SMTP_PORT', 25),
+    //     secure: env('EMAIL_SMTP_SECURE', false),
+    //     auth: {
+    //       user: env('EMAIL_SMTP_USER'),
+    //       pass: env('EMAIL_SMTP_PASS'),
+    //     },
+    //     ignoreTLS: true,
+    //   },
       settings: {
         defaultFrom: 'noreply@rumahdigitalgombengsari.com',
         defaultReplyTo: 'noreply@rumahdigitalgombengsari.com',
